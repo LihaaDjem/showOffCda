34a9456d72e469f7a7f99ef18d14a9827ed989f5
LihaaDjem <aboosowet@gmail.com>
2025-02-16 19:42:55 +0100

    ajout des cards

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('section');
-
-    sections.forEach(section => {
-        const h2 = section.querySelector('h2');
-        h2.addEventListener('click', () => {
-            const ul = section.querySelector('ul');
-            ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+    const buttons = document.querySelectorAll('.btn');
+    // sélectionne tous les boutons voir les tâches, les seuls sur la page pour le moment
+    buttons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();//empêche le comportement par défaut du lien
+            //trouve la card parente du bouton clické
+            const card = button.closest('.card');
+            //ajoute la classe "clicked" pour déclencher l'animation
+            card.classList.add('clicked');
+            //redirige vers la page des tâches après l'anim
+            setTimeout(() =>{
+                window.location.href = button.getAttribute('href');
+             }, 500);
         });
     });
 });
+    
